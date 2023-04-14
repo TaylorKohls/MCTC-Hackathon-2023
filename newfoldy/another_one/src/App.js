815d28936036626e0109b8fdcb79676b89a7c621
Noelle sdfds <you@gmail.com>
2023-04-14 10:56:20 -0500

    am

--- newfoldy/another_one/src/App.js
+++ newfoldy/another_one/src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 // import './ui_login_page.html';
 import LoginPage from './new_login.js';
 import HomePage from './home_page.js';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 //changes made here 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: < LoginPage />,   
+  },
+  {
+    path: "/home",
+    element: < HomePage />,   
+  },
+]);
+
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login_page" element={<LoginPage />}/>
-        <Route path="/home_page" element={<HomePage />} /> 
-          
-      </Routes>
-    </BrowserRouter>
-
+    <RouterProvider router={router} />
 
   );
 }
--- newfoldy/another_one/src/new_login.js
+++ newfoldy/another_one/src/new_login.js
@@ -8,31 +8,30 @@ export default function LoginPage() {
                 <div class="form-content">
                     <div class="form-items">
                         <h3>Bank name</h3>
-                        <p>Fill in the data below.</p>
+                        <p>Log in </p>
                         <form class="requires-validation" novalidate>
 
                             <div class="col-md-12">
                                <input class="form-control" type="text" name="name" placeholder="Full Name" required />
-                               <div class="valid-feedback">Username field is valid!</div>
                                <div class="invalid-feedback">Username field cannot be blank!</div>
                             </div>
 
                             <div class="col-md-12">
                                 <input class="form-control" type="email" name="email" placeholder="E-mail Address" required>
                                 </input>
-                                 <div class="valid-feedback">Email field is valid!</div>
-                                 <div class="invalid-feedback">Email field cannot be blank!</div>
+                                 <div class="invalid-feedback">Email field cannot be blank</div>
                             </div>
 
                            <div class="col-md-12">
+                           <div class="invalid-feedback">Please select an account</div>
+
                                 <select class="form-select mt-3" required>
-                                      <option selected disabled value="">Position</option>
-                                      <option value="jweb">Junior Web Developer</option>
-                                      <option value="sweb">Senior Web Developer</option>
-                                      <option value="pmanager">Project Manager</option>
+                                      <option selected disabled value="">Account</option>
+                                      <option value="jweb">Checking</option>
+                                      <option value="sweb">Savings</option>
+                                      <option value="pmanager">Credit and Loans</option>
                                </select>
-                                <div class="valid-feedback">You selected a position!</div>
-                                <div class="invalid-feedback">Please select a position!</div>
+                                <div class="valid-feedback">Login</div>
                            </div>
 
 
@@ -40,31 +39,27 @@ export default function LoginPage() {
                               <input class="form-control" type="password" name="password" placeholder="Password" required>
                               </input>
 
-                               <div class="valid-feedback">Password field is valid!</div>
-                               <div class="invalid-feedback">Password field cannot be blank!</div>
+                               <div class="invalid-feedback">Password field cannot be blank</div>
                            </div>
 
 
                            <div class="col-md-12 mt-3">
-                            <label class="mb-3 mr-1" for="gender">Gender: </label>
+                            <label class="mb-3 mr-1" for="gender">Please Select a Role: </label>
 
                             <input type="radio" class="btn-check" name="gender" id="male" autocomplete="off" required />
-                            <label class="btn btn-sm btn-outline-secondary" for="male">Male</label>
+                            <label class="btn btn-sm btn-outline-secondary" for="male">Existing</label>
 
                             <input type="radio" class="btn-check" name="gender" id="female" autocomplete="off" required />
-                            <label class="btn btn-sm btn-outline-secondary" for="female">Female</label>
+                            <label class="btn btn-sm btn-outline-secondary" for="female">New</label>
 
                             <input type="radio" class="btn-check" name="gender" id="secret" autocomplete="off" required />
                             <label class="btn btn-sm btn-outline-secondary" for="secret">Secret</label>
-                               <div class="valid-feedback mv-up">You selected a gender!</div>
-                                <div class="invalid-feedback mv-up">Please select a gender!</div>
                             </div>
 
                         <div class="form-check">
                           <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required />
 
                           <label class="form-check-label">I confirm that all data are correct</label>
-                         <div class="invalid-feedback">Please confirm that the entered data are all correct!</div>
                         </div>
                   
 
